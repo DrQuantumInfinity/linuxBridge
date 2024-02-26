6c4f2ef1d9d6a4d277b01937d59c3701bde02c00
Paul <paulbanwell@beyondaerospace.com>
2024-02-25 22:23:58 -0800

    add mqtt wrapper

---
+++ examples/bridge-app/linux/mqtt/mqtt.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <mosquitto.h>
+
+typedef void (*mqtt_msgCallback)(const struct mosquitto_message *);
+
+typedef struct mqtt_inst
+{
+    mosquitto * mosq;
+    mqtt_msgCallback messageHandler;
+    std::unordered_set<char*> subs;
+} mqtt_inst_t;
+
+
+void add_sub(mqtt_inst * inst, char * topic);
+void unsub(mqtt_inst * inst, char * topic);
